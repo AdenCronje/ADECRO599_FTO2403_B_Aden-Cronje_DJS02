487d7ea57781bd817f7ac3f9e8c6f250d04dd160
Aden Cronje <adencronje05@gmail.com>
2024-07-11 16:41:02 +0200

    Added if statement to check if inputs are empty

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+const calculateButton = document.querySelector("button[type=submit]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  // Checking if input is empty
+  if (dividend.length === 0 || divider.length === 0) {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+  }
+  if (divider === 0) {
+    
+  }
+
   // Added Math.trunc() to remove decimals
   result.innerText = Math.trunc(dividend / divider);
 });
 
 // Check for when nothing is put in input fields
-if (result !== null) {
-  result.innerText =
-    "Division not performed. Both values are required in inputs. Try again";
-}
-
-// An invalid division should log an error in the console
-// Providing anything that is not a number should crash the program
+// if (result !== null) {
+//   result.innerText =
+//     "Division not performed. Both values are required in inputs. Try again";
+// }
