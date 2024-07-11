e1eb8cab04a5fc4ef82c4a28098b0a585012c328
Aden Cronje <adencronje05@gmail.com>
2024-07-11 13:11:47 +0200

    Completed check to remove decimals

--- scripts.js
+++ scripts.js
@@ -5,5 +5,15 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  // Added Math.trunc() to remove decimals
+  result.innerText = Math.trunc(dividend / divider);
 });
+
+// Check for when nothing is put in input fields
+if (result !== null) {
+  result.innerText =
+    "Division not performed. Both values are required in inputs. Try again";
+}
+
+// An invalid division should log an error in the console
+// Providing anything that is not a number should crash the program
